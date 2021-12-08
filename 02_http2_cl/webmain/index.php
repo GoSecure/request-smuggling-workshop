26aee456e4ea97838f089ea7ada92a80c669deb1
Philippe Arteau <philippe.arteau@gmail.com>
2021-12-08 01:17:26 -0500

    Application for HTTP2 HRS with exploit

---
+++ 02_http2_cl/webmain/index.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>Finance Focus</title>
+  <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700'><link rel="stylesheet" href="./static/style.css">
+
+</head>
+<body>
+<div class="wrapper">
+	<header>
+		<div class="box header-wrapper">
+			<div class="box logo">
+				<div class="logo">Finance Focus</div>
+			</div>
+			<ul class="box navigation">
+				<li><a href="#">Challenge</a></li>
+				<li><a href="#">Solution</a></li>
+				<li><a href="#">Why us?</a></li>
+				<li><a class="get-involved" href="#">Get Involved</a></li>
+			</ul>
+		</div>
+	</header>
+	<main>
+		<section id="section-1" class="box">
+			<div class="box section-wrapper">
+				<h1 class="box main-title">Finance Focus</h1>
+				<p class="box main-subtitle">The future of agile market-driven blockchain monitoring</p>
+				<form class="box main-form" action="/" autocomplete="off" method="post" name="" >
+					<input class="box input-1" type="email" name="email" value="" autocomplete="off" placeholder="Please enter your email">
+					<div style="position: absolute; left: -5000px;" aria-hidden="true">
+						<input type="text" name="" value="">
+					</div>
+					<input class="box input-2" type="submit" name="subscribe" value="Early Access">
+				</form>
+				<div class="main-offer">
+					<div class="main-offer-section">
+						<input type="radio" name="special_offer">
+						<div class="main-offer-item1">First 500 our members will get a 50% discount.</div>
+					</div>
+					<div class="main-offer-section">
+						<input type="radio" name="special_offer">
+						<div class="main-offer-item2">Private beta coming this Winter.</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section id="section-2" class="box">
+			<div class="box section-2-wrapper">
+				<div class="section-2-content">
+					<div class="section-2-content-1">
+						<h2 class="section-2-title">Operational agility</h2>
+						<p class="section-2-text">As businesses adapt to an ever-changing new normal, extended collaboration and optimization beyond your organization is essential for next-level growth. Transformation isn’t always easy, and technology shouldn’t stand in the way. Drive operational agility and open new revenue streams through trusted data exchange and workflow automation beyond your organization boundaries. .</p>
+						<p class="section-2-text">Learn how Finance Focus is leading the business world into a new era of collaboration and innovation</p>
+					</div>
+					<div class="section-2-content-2">
+						<img src="./static/diagram_full_size.png" alt="diagram_full_size"></img>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section id="section-3" class="box">
+			<h2 class="section-3-title">Our Mission</h2>
+			<p class="section-3-text">Enable trusted data exchange and workflow automation beyond the boundaries with distributed ledger technology and blockchain</p>
+			<div class="section-3-wrapper">
+					<div class="section-3-inside">
+						<div class="section-3-inside-item"></div>
+						<span>Data</span>
+						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, sapiente.</p>
+					</div>
+					<div class="section-3-inside">
+						<div class="section-3-inside-item"></div>
+						<span>Trends and growth</span>
+						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, nihil.</p>
+					</div>
+					<div class="section-3-inside">
+						<div class="section-3-inside-item"></div>
+						<span>Notifications</span>
+						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, nam.</p>
+					</div>
+			</div>
+		</section>
+		<section id="section-4" class="box">
+			<div class="box section-4-wrapper">
+				<div class="box section-4-image" style="background-image:url(./static/blockchain.jpg)"><img src=""></div>
+				<div class="box section-4-text-block">
+					<h2 class="section-4-title">What is a blockchain?</h2>
+					<p class="section-4-text">Blockchain is a shared, immutable ledger that facilitates the process of recording transactions and tracking assets in a business network. An asset can be tangible (a house, car, cash, land) or intangible (intellectual property, patents, copyrights, branding). Virtually anything of value can be tracked and traded on a blockchain network, reducing risk and cutting costs for all involved.</p>
+					<p class="section-4-text">Also, it is a buzzword to impress your users.</p>
+				</div>
+			</div>
+		</section>
+	</main>
+	<footer>
+		<div class="box footer-wrapper">
+			<ul class="box footer-section">
+				<li class="box footer-about">
+					<div class="logo">Logo</div>
+				</li>
+				<li class="box footer-about"><strong>About</strong>
+					<ul>
+						<li class="box footer-section-items"><a href="#">Blog</a></li>
+						<li class="box footer-section-items"><a href="#">Twitter</a></li>
+						<li class="box footer-section-items"><a href="#">Facebook</a></li>
+					</ul>
+				</li>
+				<li class="box footer-about">
+					<div>
+						&copy; <?php echo date("Y"); ?> Company
+					</div>
+				</li>
+			</ul>
+		</div>
+	</footer>
+</div>
+<!-- partial -->
+  
+</body>
+</html>
