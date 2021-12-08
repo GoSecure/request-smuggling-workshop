1c11937909348c4054c2f32e33325177e26e30ef
Philippe Arteau <philippe.arteau@gmail.com>
2021-12-08 13:26:25 -0500

    Updating websocket exercise to make use of SSRF. (Instead of reflected status code)

--- 03_websocket/boot-websocket/src/main/java/net/gosecure/websocket/config/HealthCheckController.java
+++ 03_websocket/boot-websocket/src/main/java/net/gosecure/websocket/config/HealthCheckController.java
@@ -1,10 +1,13 @@
 package net.gosecure.websocket.config;
 
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.util.Map;
 
 import static org.springframework.http.MediaType.TEXT_PLAIN;
 
@@ -12,7 +15,42 @@ import static org.springframework.http.MediaType.TEXT_PLAIN;
 public class HealthCheckController {
 
     @RequestMapping("/health-check")
-    public ResponseEntity<String> health(@RequestParam("url") String url, @RequestParam("code") int statusCode) {
+    public ResponseEntity<String> health(@RequestParam("url") String url,
+                                         @RequestParam(value = "code", required = false) Integer statusCodeInput)
+            throws IOException {
+
+//        String url = body.get("url");
+//        Integer statusCodeInput = Integer.parseInt(body.get("code"));
+
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        if(url.equals("http://store.initech.com")) {
+            statusCodeInput = 200;
+        }
+        else if(url.equals("http://tickets.initech.com")) {
+            statusCodeInput = 200;
+        }
+        else if(url.equals("http://blog.initech.com")) {
+            statusCodeInput = 500;
+        }
+
+
+        int statusCode;
+
+
+        if(statusCodeInput != null) {
+            statusCode = statusCodeInput;
+        }
+        else {
+            try (Response response = client.newCall(request).execute()) {
+                statusCode = response.code();
+            }
+        }
+
         return ResponseEntity
                 .status(statusCode)
                 .contentType(TEXT_PLAIN)
---
+++ 03_websocket_statuscode_helper/html/index.php
@@ -0,0 +1,5 @@
+<?php
+http_response_code(101);
+#print_r($_SERVER);
+echo "OK";
+?>
